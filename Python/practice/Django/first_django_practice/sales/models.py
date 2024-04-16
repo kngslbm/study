86a565626c04c96aabcab051ac729bdf946331be
kngslbm <kangseulbeom@gmail.com>
2024-04-16 14:39:08 +0900

    model 수정 -> DB 구조 변경 -> migrate 과정 재실시

--- Python/practice/Django/first_django_practice/sales/models.py
+++ Python/practice/Django/first_django_practice/sales/models.py
@@ -1,12 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
 
 
 class Sale(models.Model):
     name = models.CharField(max_length=30)
     age = models.IntegerField(default=0)
-    person = models.ForeignKey("Person", on_delete=models.CASCADE)
-
-
-class Person(models.Model):
-    name = models.CharField(max_length=30)
-    age = models.IntegerField(default=0)
+    person = models.ForeignKey("User", on_delete=models.CASCADE)
