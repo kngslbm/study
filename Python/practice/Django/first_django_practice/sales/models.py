55d461ab5cf2bd8768c27bb5cdcc9a981534dc74
kngslbm <kangseulbeom@gmail.com>
2024-04-16 12:44:51 +0900

    ForeignKey 설정 (Sale 은 Person을 ForeignKey로 참조한다)

--- Python/practice/Django/first_django_practice/sales/models.py
+++ Python/practice/Django/first_django_practice/sales/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Sale(models.Model):
     name = models.CharField(max_length=30)
     age = models.IntegerField(default=0)
+    person = models.ForeignKey("Person", on_delete=models.CASCADE)
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField(default=0)
