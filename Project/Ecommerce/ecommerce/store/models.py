d39eb25a09b346c1b7b8ffb32791f6450d98c8b9
kngslbm <kangseulbeom@gmail.com>
2024-04-19 17:54:20 +0900

    @property 사용해서 html에 render(?) date

--- Project/Ecommerce/ecommerce/store/models.py
+++ Project/Ecommerce/ecommerce/store/models.py
@@ -14,12 +14,20 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
-    price = models.FloatField()
+    price = models.IntegerField()
     digital = models.BooleanField(default=False, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 
 class Order(models.Model):
@@ -32,6 +40,18 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
 
 class OrderItem(models.Model):
     product = models.ForeignKey(
@@ -43,6 +63,11 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
 class ShippingAddress(models.Model):
