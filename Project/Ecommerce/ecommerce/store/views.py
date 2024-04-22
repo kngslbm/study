22783167024e9e6218e15af5eee1e6006cdc4118
kngslbm <kangseulbeom@gmail.com>
2024-04-22 12:09:21 +0900

    배송이 필요한 상품 주문시에만 shipping info 표시.

--- Project/Ecommerce/ecommerce/store/views.py
+++ Project/Ecommerce/ecommerce/store/views.py
@@ -20,7 +20,7 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
 
     context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
@@ -34,7 +34,7 @@ def checkout(request):
         items = order.orderitem_set.all()
     else:
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
 
     context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
