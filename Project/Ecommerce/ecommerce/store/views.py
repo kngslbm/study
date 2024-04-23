8276f839d301feefd80efc2d629e48a68b94a095
kngslbm <kangseulbeom@gmail.com>
2024-04-23 19:44:55 +0900

    cookie and token

--- Project/Ecommerce/ecommerce/store/views.py
+++ Project/Ecommerce/ecommerce/store/views.py
@@ -19,9 +19,39 @@ def cart(request):
         order, created = Order.objects.get_or_create(
             customer=customer, complete=False)
         items = order.orderitem_set.all()
+
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
+
+        try:
+            for i in cart:
+                product = Product.objects.get(id=i)
+                total = (product.price * cart[i]["quantity"])
+
+                order['get_cart_total'] += total
+                order['get_cart_items'] += cart[i]["quantity"]
+
+                item = {
+                    'product': {
+                        'id': product.id,
+                        'name': product.name,
+                        'price': product.price,
+                        'imageURL': product.imageURL,
+                    },
+                    'quantity': cart[i]["quantity"],
+                    'get_total': total
+                }
+                items.append(item)
+
+                if product.digital == False:
+                    order['shipping'] = True
+        except:
+            pass
 
     context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
@@ -33,6 +63,7 @@ def checkout(request):
         order, created = Order.objects.get_or_create(
             customer=customer, complete=False)
         items = order.orderitem_set.all()
+
     else:
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
